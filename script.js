ccca5c7e239d7e96a78b96cbe2a09ef0c1b9c9e5
RoxyByProxy <116697437+RoxyByProxy@users.noreply.github.com>
2023-01-04 14:43:36 -0500

    Create outline of program in comments

---
+++ script.js
@@ -0,0 +1,12 @@
+//1 create basic functions
+    //1.a add
+    //1.b subtract
+    //1.c multiply
+    //1.d divide
+//2 create operate function
+//4 populate display when numbers are pressed
+//5 hook operate() into calculator buttons and display
+    //5.a this is the hardest part
+//ec.1 create decimal button and handling
+//ec.3 create backspace button
+//ec.4 add keyboard support
