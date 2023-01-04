69a3141daf16e687427b7c631d39b358db4ab33e
RoxyByProxy <116697437+RoxyByProxy@users.noreply.github.com>
2023-01-04 15:02:26 -0500

    create basic math functions

--- script.js
+++ script.js
@@ -10,3 +10,21 @@
 //ec.1 create decimal button and handling
 //ec.3 create backspace button
 //ec.4 add keyboard support
+
+//start of mathematical functions
+//a and b will be used for first and second args throughout for consistancy
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return Math.floor((a / b) * 10) / 10;
+}
