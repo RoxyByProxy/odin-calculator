ed0fccdeab89f5c9a3a208637ff328b9626a9409
RoxyByProxy <116697437+RoxyByProxy@users.noreply.github.com>
2023-01-04 16:50:26 -0500

    Add functionality to make calculator work Add operate() and populate() while creating placeholder variables and adding event listeners to buttons in hopes of making basic functionality

--- script.js
+++ script.js
@@ -11,6 +11,16 @@
 //ec.3 create backspace button
 //ec.4 add keyboard support
 
+//hook into html elements for later
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+//initalize variables I'll need later
+let current = null;
+let alpha = null;
+let op = null;
+let beta = null;
+
 //start of mathematical functions
 //a and b will be used for first and second args throughout for consistancy
 function add(a, b) {
@@ -27,4 +37,69 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return Math.floor((a / b) * 10) / 10;
+}
+
+//this is the heavy-lifter function for the rest of the program
+function operate(a, operator, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            console.log(`ERROR: operate was called without a valid operator, value a: ${a} b: ${b} operator: ${operator}`);
+            return false;
+    }
+}
+
+//populates the display when numbers are pressed
+function populate(input) {
+    //checks for special cases
+    if (input === 'C') {
+        //resets all values to default
+        alpha = null;
+        op = null;
+        beta = null;
+        return;
+    }
+    else if (input === '=') {
+        //performs operation
+        const result = operate(alpha, op, beta);
+        //then sets alpha to the result and restores other values to default
+        alpha = result;
+        op = null;
+        beta = null;
+        //displays result and breaks
+        display.textContent = result;
+        return;
+    }
+    //otherwise performs normal operations
+    else {
+        if (op === null) {
+            alpha += input;
+            display.textContent = alpha;
+            return;
+        }
+        else if (input === '+' || input === '-' || input === '*' || input === '/') {
+            op = input;
+            display.textContent = op;
+            return;
+        }
+        else {
+            beta += input;
+            display.textContent = beta;
+            return;
+        }
+    }
+}
+
+//adding event listeners to buttons
+for(let button in buttons) {
+    button.addEventListener('click', () => {
+        populate(button.textContent);
+    })
 }
